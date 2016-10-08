206c5f94ad271d198db9f87b5c3c8358c591c73d
hjalmarh15 <hjelmut@gmail.com>
2016-10-08 20:13:14 +0000

    added d4

--- d0/Tester.java
+++ d0/Tester.java
@@ -1,18 +1,55 @@
 package d0;
+import java.awt.event.KeyEvent;
 
-import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdDraw;
+public class Tester { 
 
-public class Tester {
-	public static void main(String[] args) {
-		int sum = 0;
-		int n = 3000;
-		
-		for (int i=0; i < n; i++) {
-		        for (int j=0; j < i; j+=2) {
-		           sum++;
-		        }
-		}
-		
-		StdOut.println(sum);
-	}
-}
+    public static void main(String[] args) {
+
+        // set the scale of the coordinate system
+        StdDraw.setXscale(-1.0, 1.0);
+        StdDraw.setYscale(-1.0, 1.0);
+        StdDraw.enableDoubleBuffering();
+
+        // initial values
+        double rx = Math.random()*0.480;
+        double ry = Math.random()*0.860;     // position
+        double vx = Math.random()*0.1;
+        double vy = Math.random()*0.2;     // velocity
+        double radius = 0.05;              // radius
+
+        // main animation loop
+        while (true)  { 
+
+            // bounce off wall according to law of elastic collision
+            if (Math.abs(rx + vx) > 1.0 - radius) vx = -vx;
+            if (Math.abs(ry + vy) > 1.0 - radius) vy = -vy;
+
+            // update position
+            rx = rx + vx; 
+            ry = ry + vy; 
+
+            // clear the background
+            StdDraw.clear(StdDraw.PINK);
+
+            // draw ball on the screen
+            //String ballCountString = this.getParameter(20);
+            for(int i=0; i< 20;i++) {
+                StdDraw.setPenColor(StdDraw.BLUE); 
+                StdDraw.filledCircle(rx, ry, radius); 
+
+            // copy offscreen buffer to onscreen
+            StdDraw.show();
+
+            // pause for 20 ms
+            StdDraw.pause(20);
+
+            // Press Space to bring balls to center
+            
+            }
+            
+                
+            
+        } 
+    } 
+} 
---
+++ d4/BreadthFirstPaths.java
@@ -0,0 +1,293 @@
+/******************************************************************************
+ *  Compilation:  javac BreadthFirstPaths.java
+ *  Execution:    java BreadthFirstPaths G s
+ *  Dependencies: Graph.java Queue.java Stack.java StdOut.java
+ *  Data files:   http://algs4.cs.princeton.edu/41graph/tinyCG.txt
+ *                http://algs4.cs.princeton.edu/41graph/tinyG.txt
+ *                http://algs4.cs.princeton.edu/41graph/mediumG.txt
+ *                http://algs4.cs.princeton.edu/41graph/largeG.txt
+ *
+ *  Run breadth first search on an undirected graph.
+ *  Runs in O(E + V) time.
+ *
+ *  %  java Graph tinyCG.txt
+ *  6 8
+ *  0: 2 1 5 
+ *  1: 0 2 
+ *  2: 0 1 3 4 
+ *  3: 5 4 2 
+ *  4: 3 2 
+ *  5: 3 0 
+ *
+ *  %  java BreadthFirstPaths tinyCG.txt 0
+ *  0 to 0 (0):  0
+ *  0 to 1 (1):  0-1
+ *  0 to 2 (1):  0-2
+ *  0 to 3 (2):  0-2-3
+ *  0 to 4 (2):  0-2-4
+ *  0 to 5 (1):  0-5
+ *
+ *  %  java BreadthFirstPaths largeG.txt 0
+ *  0 to 0 (0):  0
+ *  0 to 1 (418):  0-932942-474885-82707-879889-971961-...
+ *  0 to 2 (323):  0-460790-53370-594358-780059-287921-...
+ *  0 to 3 (168):  0-713461-75230-953125-568284-350405-...
+ *  0 to 4 (144):  0-460790-53370-310931-440226-380102-...
+ *  0 to 5 (566):  0-932942-474885-82707-879889-971961-...
+ *  0 to 6 (349):  0-932942-474885-82707-879889-971961-...
+ *
+ ******************************************************************************/
+
+package d4;
+
+import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.Graph;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+/**
+ *  The {@code BreadthFirstPaths} class represents a data type for finding
+ *  shortest paths (number of edges) from a source vertex <em>s</em>
+ *  (or a set of source vertices)
+ *  to every other vertex in an undirected graph.
+ *  <p>
+ *  This implementation uses breadth-first search.
+ *  The constructor takes time proportional to <em>V</em> + <em>E</em>,
+ *  where <em>V</em> is the number of vertices and <em>E</em> is the number of edges.
+ *  It uses extra space (not including the graph) proportional to <em>V</em>.
+ *  <p>
+ *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/41graph">Section 4.1</a>   
+ *  of <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
+ */
+public class BreadthFirstPaths {
+    private static final int INFINITY = Integer.MAX_VALUE;
+    private boolean[] marked;  // marked[v] = is there an s-v path
+    private int[] edgeTo;      // edgeTo[v] = previous edge on shortest s-v path
+    private int[] distTo;      // distTo[v] = number of edges shortest s-v path
+    private int[] nrOfPaths;
+    /**
+     * Computes the shortest path between the source vertex {@code s}
+     * and every other vertex in the graph {@code G}.
+     * @param g the graph
+     * @param s the source vertex
+     */
+    public BreadthFirstPaths(Digraph g, int s) {
+        marked = new boolean[g.V()];
+        distTo = new int[g.V()];
+        edgeTo = new int[g.V()];
+        nrOfPaths = new int[g.V()];
+        bfs(g, s);
+
+        assert check(g, s);
+    }
+
+    /**
+     * Computes the shortest path between any one of the source vertices in {@code sources}
+     * and every other vertex in graph {@code G}.
+     * @param G the graph
+     * @param sources the source vertices
+     */
+    public BreadthFirstPaths(Graph G, Iterable<Integer> sources) {
+        marked = new boolean[G.V()];
+        distTo = new int[G.V()];
+        edgeTo = new int[G.V()];
+        nrOfPaths = new int[G.V()];
+        for (int v = 0; v < G.V(); v++)
+            distTo[v] = INFINITY;
+        bfs(G, sources);
+    }
+
+
+    // breadth-first search from a single source
+    private void bfs(Digraph g, int s) {
+        Queue<Integer> q = new Queue<Integer>();
+        for (int v = 0; v < g.V(); v++) {
+        	distTo[v] = INFINITY;
+        	nrOfPaths[v] = 0;
+        	marked[v] = false;
+        }
+        
+        nrOfPaths[s] = 1;
+        marked[s] = true;
+        q.enqueue(s);
+        while (!q.isEmpty()) {
+            int v = q.dequeue();
+            for (int w : g.adj(v)) {
+                if (!marked[w]) {
+                    edgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                    marked[w] = true;
+                    q.enqueue(w);
+                }
+                if (distTo[w] == distTo[v] + 1) {
+                	nrOfPaths[w] = nrOfPaths[w] + nrOfPaths[v];
+                }
+            }
+        }
+      }
+
+    // breadth-first search from multiple sources
+    private void bfs(Graph G, Iterable<Integer> sources) {
+        Queue<Integer> q = new Queue<Integer>();
+        for (int s : sources) {
+            marked[s] = true;
+            distTo[s] = 0;
+            q.enqueue(s);
+        }
+        while (!q.isEmpty()) {
+            int v = q.dequeue();
+            for (int w : G.adj(v)) {
+                if (!marked[w]) {
+                    edgeTo[w] = v;
+                    distTo[w] = distTo[v] + 1;
+                    marked[w] = true;
+                    q.enqueue(w);
+                }
+            }
+        }
+    }
+
+    /**
+     * Is there a path between the source vertex {@code s} (or sources) and vertex {@code v}?
+     * @param v the vertex
+     * @return {@code true} if there is a path, and {@code false} otherwise
+     */
+    public boolean hasPathTo(int v) {
+        return marked[v];
+    }
+    
+    public int nrOfPathsTo(int v){
+    	//return number of shortest paths
+    	return nrOfPaths[v];
+    }
+
+    /**
+     * Returns the number of edges in a shortest path between the source vertex {@code s}
+     * (or sources) and vertex {@code v}?
+     * @param v the vertex
+     * @return the number of edges in a shortest path
+     */
+    public int distTo(int v) {
+        return distTo[v];
+    }
+
+    /**
+     * Returns a shortest path between the source vertex {@code s} (or sources)
+     * and {@code v}, or {@code null} if no such path.
+     * @param v the vertex
+     * @return the sequence of vertices on a shortest path, as an Iterable
+     */
+    public Iterable<Integer> pathTo(int v) {
+        if (!hasPathTo(v)) return null;
+        Stack<Integer> path = new Stack<Integer>();
+        int x;
+        for (x = v; distTo[x] != 0; x = edgeTo[x])
+            path.push(x);
+        path.push(x);
+        return path;
+    }
+
+
+    // check optimality conditions for single source
+    private boolean check(Digraph g, int s) {
+
+        // check that the distance of s = 0
+        if (distTo[s] != 0) {
+            StdOut.println("distance of source " + s + " to itself = " + distTo[s]);
+            return false;
+        }
+
+        // check that for each edge v-w dist[w] <= dist[v] + 1
+        // provided v is reachable from s
+        for (int v = 0; v < g.V(); v++) {
+            for (int w : g.adj(v)) {
+                if (hasPathTo(v) != hasPathTo(w)) {
+                    StdOut.println("edge " + v + "-" + w);
+                    StdOut.println("hasPathTo(" + v + ") = " + hasPathTo(v));
+                    StdOut.println("hasPathTo(" + w + ") = " + hasPathTo(w));
+                    return false;
+                }
+                if (hasPathTo(v) && (distTo[w] > distTo[v] + 1)) {
+                    StdOut.println("edge " + v + "-" + w);
+                    StdOut.println("distTo[" + v + "] = " + distTo[v]);
+                    StdOut.println("distTo[" + w + "] = " + distTo[w]);
+                    return false;
+                }
+            }
+        }
+
+        // check that v = edgeTo[w] satisfies distTo[w] = distTo[v] + 1
+        // provided v is reachable from s
+        for (int w = 0; w < g.V(); w++) {
+            if (!hasPathTo(w) || w == s) continue;
+            int v = edgeTo[w];
+            if (distTo[w] != distTo[v] + 1) {
+                StdOut.println("shortest path edge " + v + "-" + w);
+                StdOut.println("distTo[" + v + "] = " + distTo[v]);
+                StdOut.println("distTo[" + w + "] = " + distTo[w]);
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Unit tests the {@code BreadthFirstPaths} data type.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+    	//new In stream
+    	In in = new In();
+    	
+    	//new Digraph takes in In stream
+    	Digraph graph = new Digraph(in);
+    	
+    	//number of test cases
+    	int s = in.readInt();
+    	for(int i = 0; i < s; i++) {
+    		//first number of the set
+	    	int v = in.readInt();
+	    	//second number of the set
+	    	int w = in.readInt();
+	    	BreadthFirstPaths bfs = new BreadthFirstPaths(graph, v);
+	    	
+	    	//print number of shortest paths
+	    	StdOut.println(bfs.nrOfPathsTo(w));
+    	}
+    	
+    }
+
+
+}
+
+/******************************************************************************
+ *  Copyright 2002-2016, Robert Sedgewick and Kevin Wayne.
+ *
+ *  This file is part of algs4.jar, which accompanies the textbook
+ *
+ *      Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne,
+ *      Addison-Wesley Professional, 2011, ISBN 0-321-57351-X.
+ *      http://algs4.cs.princeton.edu
+ *
+ *
+ *  algs4.jar is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  algs4.jar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with algs4.jar.  If not, see http://www.gnu.org/licenses.
+ ******************************************************************************/
